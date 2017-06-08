2facc2b8e994c3bdee60186550eea97cdbe49e7f
seunghkong <seunghkong@gmail.com>
2017-06-08 16:26:46 +0900

    add

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//
+ //
 //  main.c
 //  jagu
 //
@@ -13,49 +13,49 @@ typedef struct rbNode* NodePtr;
 struct rbNode{
     int key;
     int red;
-    NodePtr left, right, parent;
+    NodePtr left,
+            right,
+            parent;
 };
+
+NodePtr nil;
 
 typedef struct rbtree* TreePtr;
 struct rbtree {
-    NodePtr root, sentinel;
+    NodePtr nil, root;
+    
 };
+
+NodePtr nilinit(){
+    nil = (NodePtr)malloc(sizeof(struct rbNode));
+    nil->key = -1;
+    nil->red = 0;
+    nil->left = nil->right = NULL;
+
+    return nil;
+}
 
 NodePtr nodeinit(int key){
     NodePtr node = (NodePtr)malloc(sizeof(struct rbNode));
-    node->left = NULL;
-    node->right = NULL;
-    node->parent = NULL;
     node->key = key;
+    node->left = nil;
+    node->right = nil;
+    node->parent = nil;
+    //if (key == -1){
+      //  node->red = 0;
+    //}
     return node;
 };
 
 TreePtr treeinit(){
-    TreePtr self = (TreePtr)malloc(sizeof(struct rbtree));
-    //TreePtr tree = NULL;
-    //NodePtr node = nodeinit();
-    //node->red = 0;
-    self->root = NULL;
-    self->sentinel = NULL;
-    return self;
+    TreePtr tree = (TreePtr)malloc(sizeof(struct rbtree));
+    tree->root = nil;
+    tree->nil = nil;
+    return tree;
 }
 
-NodePtr rbmin(NodePtr node, NodePtr x){
-    while (node->left != x){
-        node = node->left;
-    }
-    return x;
-}
-
-NodePtr rbmax(NodePtr node, NodePtr x){
-    while (node->right != x){
-        node = node->left;
-    }
-    return node;
-}
-
-NodePtr nodemin(NodePtr node, NodePtr sentinel){
-    while (node->left != sentinel){
+NodePtr nodemin(NodePtr node, NodePtr nil){
+    while (node->left != nil){
         node = node->left;
     }
     return node;
@@ -63,126 +63,123 @@ NodePtr nodemin(NodePtr node, NodePtr sentinel){
 
 NodePtr treemin(TreePtr tree){
     NodePtr node = tree->root;
-    NodePtr sentinel = tree->sentinel;
-    if (node == sentinel){
-        return NULL;
+    NodePtr nil = tree->nil;
+    if (node == nil){
+        return nil;
     } else {
-        return nodemin(node, sentinel);
+        return nodemin(node, nil);
     }
 }
 
-void left_Rot(TreePtr root, NodePtr x, NodePtr node){
-    NodePtr right = node->right;
-    node->right = right->left;
-    if(right->left != x){
-        right->left->parent = node;
+void left_Rot(TreePtr t, NodePtr x){
+    NodePtr y = x->right;
+    x->right = y->left;
+    if (y->left != t->nil) {
+        y->left->parent = x;
     }
-    right->parent = node->parent;
-    if (node == root->root){
-        root->root = right;
-    } else if ( node == node->parent->left){
-        node->parent->left = right;
+    y->parent = x->parent;
+    if (x->parent == t->nil) {
+        t->root = y;
+    } else if (x == x->parent->left) {
+        x->parent->left = y;
     } else {
-        node->parent->right = right;
+        x->parent->right = y;
     }
-    right->left = node;
-    node->parent = right;
-    return;
+    y->left = x;
+    x->parent = y;
 }
 
-void right_Rot(TreePtr root, NodePtr x, NodePtr node){
-    NodePtr left = node->left;
-    node->left = left->right;
-    
-    if(left->right != x){
-        left->right->parent = node;
+void right_Rot(TreePtr t, NodePtr y){
+    NodePtr x = y->left;
+    y->left = x->right;
+    if (x->right != t->nil) {
+        x->right->parent = y;
     }
-    
-    left->parent = node->parent;
-    if(node == root->root){
-        root->root = left;
-    } else if (node == node->parent->right){
-        node->parent->right = left;
+    x->parent = y->parent;
+    if (y->parent == t->nil) {
+        t->root = x;
+    } else if (y == y->parent->left) {
+        y->parent->left = x;
     } else {
-        node->parent->left = left;
+        y->parent->right = x;
     }
-    left->right = node;
-    node->parent = left;
-    return;
+    x->right = y;
+    y->parent = x;
 }
 
-void InsertFix(TreePtr tree, NodePtr z){
-    while (z->parent != NULL && z->parent->red == 1){
+void InsertFix(TreePtr t, NodePtr z){
+    NodePtr y;
+    while (z->parent->red) {
         if (z->parent == z->parent->parent->left) {
-            NodePtr y = z->parent->parent->right;
-            if (y->red){
+            y = z->parent->parent->right;
+            if (y != t->nil && y->red){
                 z->parent->red = 0;
                 y->red = 0;
                 z->parent->parent->red = 1;
                 z = z->parent->parent;
             } else {
-                if (z == z->parent->right){
-                    z = z->parent;
-                    left_Rot(tree, tree->sentinel, z);
+                if (z == z->parent->right) {
+                z = z->parent;
+                left_Rot(t, z);
                 }
-                z->parent->red = 0;
-                z->parent->parent->red = 1;
-                right_Rot(tree, tree->sentinel, z->parent->parent);
+            z->parent->red = 0;
+            z->parent->parent->red = 1;
+            right_Rot(t, z->parent->parent);
             }
         } else {
-            NodePtr y = z->parent->parent->left;
-            if (y->red){
+            y = z->parent->parent->left;
+            if (y != t->nil && y->red) {
                 z->parent->red = 0;
                 y->red = 0;
-                z->parent->parent->red = 1;
+                z->parent->parent->red = 0;
                 z = z->parent->parent;
             } else {
-                if(z== z->parent->left){
+                if (z == z->parent->left) {
                     z = z->parent;
-                    left_Rot(tree, tree->sentinel, z);
+                    right_Rot(t, z);
                 }
                 z->parent->red = 0;
                 z->parent->parent->red = 1;
-                right_Rot(tree, tree->sentinel, z->parent->parent);
+                left_Rot(t, z->parent->parent);
             }
         }
     }
-    tree->root->red = 0;
+    t->root->red = 0;
 }
 
-void insert(TreePtr tree, NodePtr z){
-    NodePtr x = tree->root;
-    NodePtr y = tree->sentinel;
-    //NodePtr parent, samchon, *p;
+void insert(TreePtr t, NodePtr z){
+    NodePtr y = t->nil;
+    NodePtr x = t->root;
     
-    if (x == tree->sentinel){
-        z->parent = NULL;
-        z->left = tree->sentinel;
-        z->right = tree->sentinel;
+    if (x == NULL){
+        z->parent = nil;
+        z->left = t->nil;
+        z->right = t->nil;
         z->red = 0;
-        tree->root = z;
+        t->root = z;
         return;
     }
     
-    while (x != tree->sentinel){
+    while (x != t->nil) {
         y = x;
-        if (z->key < x->key){
+        if (z->key < x->key) {
             x = x->left;
         } else {
             x = x->right;
         }
     }
-    if (y == tree->sentinel){
-        tree->root = z;
+    z->parent = y;
+    if (y == t->nil) {
+        t->root = z;
     } else if (z->key < y->key) {
         y->left = z;
     } else {
         y->right = z;
     }
-    z->left = tree->sentinel;
-    z->right = tree->sentinel;
+    z->left  = t->nil;
+    z->right = t->nil;
     z->red = 1;
-    InsertFix(tree, z);
+    InsertFix(t, z);
 }
 
 
@@ -196,195 +193,188 @@ char choosecol(int red){
     }
 }
 
-void printtree(NodePtr root){
-    NodePtr temp = root;
-    if (temp != NULL){
-        printtree(temp->left);
-        printf("%d-%c ", temp->key, choosecol(temp->red));
-        printtree(temp->right);
+NodePtr search(TreePtr t, NodePtr node, int val){
+    if (node==t->nil || node->key == val){
+        return node;
     }
-    return;
-}
-
-NodePtr search(NodePtr root, int val){
-    if (root==NULL || root->key == val){
-        return root;
-    }
-    if (root->key < val){
-        return search(root->right, val);
+    if (node->key < val){
+        return search(t, node->right, val);
     } else {
-        return search(root->left, val);
+        return search(t, node->left, val);
     }
 }
 
-void transplant(TreePtr self, NodePtr tree, NodePtr v){
-    if (tree->parent == NULL)
-        self->root= v;
-    else if (tree==tree->parent->left)
-        tree->parent->left=v;
-    else
-        tree->parent->right= v;
-    if (v!=NULL)
-        v->parent = tree->parent;
+void transplant(TreePtr t, NodePtr u, NodePtr v){
+    if (u->parent == nil) {
+        t->root = v;
+    } else if (u == u->parent->left) {
+        u->parent->left = v;
+    } else {
+        u->parent->right = v;
+    }
+    v->parent = u->parent;
 }
 
-void deletefix(TreePtr tree, NodePtr x){
+void deletefix(TreePtr t, NodePtr x) {
     NodePtr w;
-    while (x != tree->root && x->red == 0){
-        if (x == x->parent->left){
+    while (x != t->root && x->red == 0) {
+        if (x == x->parent->left) {
             w = x->parent->right;
-            if (w->red){
+            if (w->red) {
                 w->red = 0;
                 x->parent->red = 1;
-                left_Rot(tree, tree->sentinel, x->parent);
+                left_Rot(t, x->parent);
                 w = x->parent->right;
             }
-            if (w->left->red == 0 && w->right->red == 0){
+            if (w->left->red == 0 && w->right->red == 0) {
                 w->red = 1;
                 x = x->parent;
             } else {
-                if (w->right->red == 0){
+                if (w->right->red == 0) {
                     w->left->red = 0;
                     w->red = 1;
-                    right_Rot(tree, tree->sentinel, w);
+                    right_Rot(t, w);
+                    w = x->parent->right;
                 }
+            
                 w->red = x->parent->red;
                 x->parent->red = 0;
                 w->right->red = 0;
-                left_Rot(tree, tree->sentinel, x->parent);
-                x = tree->root;
+                left_Rot(t, x->parent);
+                x = t->root;
             }
         } else {
             w = x->parent->left;
-            if (w->red){
+            if (w->red) {
                 w->red = 0;
                 x->parent->red = 1;
-                left_Rot(tree, tree->sentinel, x->parent);
+                right_Rot(t, x->parent);
                 w = x->parent->left;
             }
-            if (w->right->red == 0 && w->left->red == 0){
+            if (w->right->red == 0 && w->left->red == 0) {
                 w->red = 1;
                 x = x->parent;
             } else {
-                if (w->left->red == 0){
+                if (w->left->red == 0 ) {
                     w->right->red = 0;
                     w->red = 1;
-                    right_Rot(tree, tree->sentinel, w);
+                    left_Rot(t, w);
+                    w = x->parent->left;
                 }
                 w->red = x->parent->red;
                 x->parent->red = 0;
                 w->left->red = 0;
-                left_Rot(tree, tree->sentinel, x->parent);
-                x = tree->root;
+                right_Rot(t, x->parent);
+                x = t->root;
             }
         }
     }
     x->red = 0;
 }
 
-void deletenode(TreePtr tree, NodePtr z){
-    
+void deletenode(TreePtr t, NodePtr z){
     NodePtr y = z;
     NodePtr x;
-    int y_original_color = y->red;
-    
-    if (z == tree->sentinel){
+    if (z == t->nil) {
         return;
-    } else if (z->left == tree->sentinel){
+    }
+    int y_original = y->red;
+    if (z->left == t->nil) {
         x = z->right;
-        transplant(tree, z, z->right);
-    } else if (z == tree->sentinel){
-        return;
-    } else if (z->right == tree->sentinel){
+        transplant(t, z, z->right);
+    } else if (z->right == t->nil) {
         x = z->left;
-        transplant(tree, z, z->left);
+        transplant(t, z, z->left);
     } else {
-        y = nodemin(z->right, tree->sentinel);
-        y_original_color = y->red;
+        y = nodemin(z->right, t->nil);
+        y_original = y->red;
         x = y->right;
-        if (y->parent == z){
+        if (y->parent == z) {
             x->parent = y;
         } else {
-            transplant(tree, y, y->right);
+            transplant(t, y, y->right);
             y->right = z->right;
             y->right->parent = y;
         }
-        transplant(tree, z, y);
+        transplant(t, z, y);
         y->left = z->left;
         y->left->parent = y;
         y->red = z->red;
     }
-    if (y_original_color == 0){
-        deletefix(tree, x);
+    if (y_original == 0) {
+        deletefix(t, x);
     }
     
 }
+
 //int height = 0;
-void rbt_print(TreePtr self, NodePtr tree, int level) {
-    if (tree->right != NULL){
+void rbt_print(TreePtr t, NodePtr node, int level) {
+    if (node->right != NULL){
         //height++;
-        rbt_print(self,tree->right, level + 1);
+        rbt_print(t, node->right, level + 1);
     }
     for(int i = 0; i < level; i++)
         printf("    ");
-    printf("%d\n", tree->key);
-    if (tree->left != NULL){
+    printf("%d-%s\n", node->key, (node->red ? "r":"b"));
+    if (node->left != NULL){
         //height++;
-        rbt_print(self, tree->left, level + 1);
+        rbt_print(t, node->left, level + 1);
     }
 }
 
-int treeheight(TreePtr tree, NodePtr node){
-    if (node == tree->sentinel){
-        return 0;
-    } else {
-        int ldepth = treeheight(tree, node->left);
-        int rdepth = treeheight(tree, node->right);
-        if (ldepth > rdepth){
-            return (ldepth+1);
-        } else {
-            return (rdepth + 1);
+int treeheight(TreePtr t){
+    NodePtr x = t->root;
+    int height = 0;
+    while (x != t->nil) {
+        x = x->left;
+        if (x->red == 0) {
+            height++;
         }
     }
+    return height;
 }
 
-int nodecount(TreePtr self, NodePtr tree) {
+int nodecount(TreePtr t, NodePtr node) {
     int i = 0;
-    if (tree == NULL)
-        return 1;
+    if (node == nil)
+        return 0;
     else {
-        i += nodecount(self, tree->left);
         
-        i += nodecount(self, tree->right);
+        i += nodecount(t, node->left);
+        i++;
+        i += nodecount(t, node->right);
     }
     return i;
 }
 
 int isleaf(TreePtr tree, NodePtr node){
-    if (node == NULL){
+    if (node == nil){
         return 0;
-    } else if (node->left == tree->sentinel){
+    } else if (node->left == tree->nil){
         return 0;
-    } else if (node->right == tree->sentinel){
+    } else if (node->right == tree->nil){
         return 0;
     } else {
         return 1;
     }
 }
 
-int blackcount(TreePtr root, NodePtr tree){
-    int i = 0;
-    if (tree == NULL){
+int blackcount(TreePtr t, NodePtr node){
+    //int i = 0;
+    if (node == nil) {
         return 0;
     } else {
-        i = isleaf(root, tree) || tree->red==0 ?  1 : 0;
-        return blackcount(root, tree->left) + blackcount(root, tree->right) + i;
+        if (node->red == 0) {
+            return blackcount(t, node->left) + blackcount(t, node->right) + 1;
+        } else {
+            return blackcount(t, node->left) + blackcount(t, node->right);
+        }
     }
     
 }
 
 void inordertrav(TreePtr tree, NodePtr node){
-    if (node == tree->sentinel){
+    if (node == tree->nil){
         return;
     } else {
         inordertrav(tree, node->left);
@@ -395,6 +385,8 @@ void inordertrav(TreePtr tree, NodePtr node){
 }
 
 int main(void){
+    //nil = nodeinit(-1);
+    nil = nilinit();
     TreePtr t = treeinit();
     FILE *fp;
     //int i = 0;
@@ -404,10 +396,16 @@ int main(void){
         if (data == 0){
             break;
         } else if (data > 0){
+            //printf("inserting %d\n",data);
             insert(t, nodeinit(data));
+            //rbt_print(t, t->root, 0);
         } else if (data < 0){
-            if (search(t->root, abs(data)) != NULL){
-                deletenode(t, search(t->root, abs(data)));
+            //printf("%d is negative\n", data);
+            if (search(t, t->root, abs(data)) != t->nil){
+                //printf("is in tree %d\n", search(t, t->root, abs(data))->key);
+                deletenode(t, search(t, t->root, abs(data)));
+                //printf("%d is deleted\n", data);
+                //rbt_print(t, t->root, 0);
             } else {
                 printf("absolute value of %d is not already in the rb Tree. Ignoring...\n", data);
             }
@@ -417,7 +415,7 @@ int main(void){
     //rbt_print(t, t->root, 0);
     printf("total = %d\n", nodecount(t, t->root));
     printf("nb = %d\n", blackcount(t, t->root));
-    printf("bh = %d\n", treeheight(t, t->root));
+    printf("bh = %d\n", treeheight(t));
     inordertrav(t, t->root);
     return 0;
 }
